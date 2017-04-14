af503c3f459b3d54b5f86b93cc8bad6d8147bf2b
Wesley <Wesley@DESKTOP-M53GS6R.attlocal.net>
2017-04-14 19:58:07 -0400

    Added getSession method to access Hibernate session object

--- src/main/java/com/wesdm/springhibernate/dao/jpa/AbstractJpaDao.java
+++ src/main/java/com/wesdm/springhibernate/dao/jpa/AbstractJpaDao.java
@@ -3,6 +3,8 @@ package com.wesdm.springhibernate.dao.jpa;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.hibernate.Session;
+
 public abstract class AbstractJpaDao {
 	@PersistenceContext
     private EntityManager em;
@@ -17,5 +19,9 @@ public abstract class AbstractJpaDao {
 	
 	public void delete(Object obj) {
 		getEntityManager().remove(obj);
+	}
+	
+	public Session getSession() {
+		return getEntityManager().unwrap(Session.class);
 	}
 }
